5e474d079e2176e1daefb53d3cfce6ab778f331d
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-04-05 23:25:39 +0530

    update profile page

--- nextjs-ecommerce-store/src/components/Usermodule/ProfileupdateForm.js
+++ nextjs-ecommerce-store/src/components/Usermodule/ProfileupdateForm.js
@@ -1,22 +1,142 @@
-import { PhotoCamera } from '@mui/icons-material'
-import { Box, Button, IconButton, Paper, Stack, TextField } from '@mui/material'
-import React from 'react'
+import { PhotoCamera } from "@mui/icons-material";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React from "react";
 
 const ProfileupdateForm = () => {
   return (
-   <Box><Paper>
-    <Stack direction="row" alignItems="center" spacing={2}>
-      <Button variant="contained" component="label">
-        Upload
-        <input hidden accept="image/*" multiple type="file" />
-      </Button>
-      <IconButton color="primary" aria-label="upload picture" component="label">
-        <input hidden accept="image/*" type="file" />
-        <PhotoCamera />
-      </IconButton>
-    </Stack>
-   </Paper> </Box>
-  )
-}
+    <Box width={"100%"}>
+      <Paper
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          py: 2,
+          gap: 2.5,
+          width: "100%",
+          p: 2,
+        }}
+      >
+        <Stack
+          direction="row"
+          sx={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            m: "0!important",
+            gap: 2.5,
+            width: "100%",
+            p: 1,
+          }}
+          alignItems="center"
+          spacing={2}
+        >
+          <Button variant="contained" sx={{textTransform:'capitalize'}} color="error" component="label">
+            Upload
+            <input hidden accept="image/*" multiple type="file" />
+          </Button>
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="label"
+          >
+            <input hidden accept="image/*" type="file" />
+            <PhotoCamera />
+          </IconButton>
+        </Stack>
+        <Grid
+          container
+          sx={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            m: "0!important",
+            gap: 2.5,
+            width: "100%",
+            p: 1,
+            "&>.MuiGrid-item": {
+              p: 0,
 
-export default ProfileupdateForm
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            },
+          }}
+          spacing={2}
+          columns={17}
+        >
+          <Grid item xs={8}>
+            <TextField
+              id="firstname"
+              name="firstname"
+              label="First Name"
+              fullWidth
+              size="small"
+              variant="outlined"
+            />
+            <TextField
+              id="emailaddress"
+              name="emailaddress"
+              label="Email Address"
+              size="small"
+              fullWidth
+              type="email"
+              variant="outlined"
+            />
+            <TextField
+              id="dateof birth"
+              label="Date of birth"
+              type="date"
+              fullWidth
+              size="small"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              id="lastname"
+              name="lastname"
+              fullWidth
+              size="small"
+              label="Last Name"
+              variant="outlined"
+            />
+            <TextField
+              id="phonenumber"
+              size="small"
+              name="phonenumber"
+              fullWidth
+              label="Phone Number"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+        <Stack sx={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            m: "0!important",
+            gap: 2.5,
+            width: "100%",
+            p: 1,
+           
+          }} direction="row" alignItems="center" spacing={2}>
+          <Button variant="contained" sx={{textTransform:'capitalize'}} color="error">
+            Save Changes
+          </Button>
+        </Stack>
+      </Paper>{" "}
+    </Box>
+  );
+};
+
+export default ProfileupdateForm;
