777c560b15d5f4cd0b83eb1d7e827764ebb4bd74
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-04-08 14:50:21 +0530

    updated code

--- strapi-ecommerce-backend/src/api/cartitem/controllers/cartitem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cartitem controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cartitem.cartitem');
