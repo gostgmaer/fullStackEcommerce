175a87a658c040c7efd0916804ff3ef21025de10
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-09-09 22:50:20 +0530

    updated

--- src/app/offer/page.js
+++ src/app/offer/page.js
@@ -1,6 +1,7 @@
 
 
 import { content } from "@/assets/jsonfile/content";
+import Coupon from "@/components/elements/coupon/Coupon";
 import PageHeading from "@/components/global/layout/heading/pageHeading";
 import Layout from "@/components/global/layout/Layout";
 
@@ -10,7 +11,7 @@ const Offer = () => {
       <PageHeading title={content["mega-offer"]} />
       <div className="mx-auto max-w-screen-2xl px-4 py-10 lg:py-20 sm:px-10">
         <div className="grid gap-6 grid-cols-1 xl:grid-cols-2">
-          {/* <Coupon /> */}
+          <Coupon couponInHome />
         </div>
       </div>
     </Layout>
