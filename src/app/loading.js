5ada3ac566f68ff0c02daf9abcec491904da2a0d
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-09-16 00:29:18 +0530

    updated

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black opacity-95 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black opacity-90 z-50">
       <div className="flex space-x-4">
         {/* Animated Dots */}
         <div className="w-4 h-4 bg-white rounded-full animate-bounce glow    delay-75"></div>
