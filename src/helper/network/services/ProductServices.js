38f781c62c19dceedd143dca5a4a180456b43b5b
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-09-20 21:59:56 +0530

    chnaged

--- src/helper/network/services/ProductServices.js
+++ src/helper/network/services/ProductServices.js
@@ -5,23 +5,23 @@ const ProductServices = {
     return await requests.get("/products", query);
   },
   getShowingProducts: async (query) => {
-    return await requests.get("/products/show", query,null,null,1);
+    return await requests.get("/products/show", query);
   },
   // getShowingStoreProducts: async (query) => {
   //   return requests.get(`/products`, query);
   // },
   getDiscountedProducts: async (query) => {
-    return requests.get("/products/discount", query,null,null,1);
+    return requests.get("/products/discount", query);
   },
   getPopularProducts: async (query) => {
-    return requests.get("/products/popular", query,null,null,1);
+    return requests.get("/products/popular", query);
   },
   getProductBySlug: async (params) => {
     return requests.get(`/products/view/:slug`, null, params,null,1);
   },
 
   getRelatedProducts: async (query) => {
-    return requests.get(`/products/related`, query,null,null,1);
+    return requests.get(`/products/related`, query);
   }
 
 };
